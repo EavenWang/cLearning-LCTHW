2b32fe6ba66964a9e304ddc411d1756dd742965d
Eaven Wang <212715685@qq.com>
2026-02-26 11:26:06 +0800

    init

---
+++ ex2/ex1.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(int argc,char *argv[]){
+
+	int age = 10;
+	puts("Hello world.");
+	return 0;
+}
---
+++ ex2/ex3.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+	int aget = 10;
+	int height = 73;
+
+	printf("I am %d years old.\n");
+	printf("I am %d inches tall.\n",height);
+	return 0;
+}
